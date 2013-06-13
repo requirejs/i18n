2bf402a4bb37a92fe812e243d1d8a6ef746bbb98
Tobias Weibel <tobias.weibel@autodesk.com>
2013-06-13 13:28:22 +0200

    Avoid exception in mixin when target[prop] == null
    When target[prop] is null then source[prop] should be assigned directly without calling mixin again. It would cause an exception because null.hasOwnProperty is undefined, of course.

--- i18n.js
+++ i18n.js
@@ -77,7 +77,12 @@
             if (source.hasOwnProperty(prop) && (!target.hasOwnProperty(prop) || force)) {
                 target[prop] = source[prop];
             } else if (typeof source[prop] === 'object') {
-                mixin(target[prop], source[prop], force);
+                if (target[prop] != null) {
+                    mixin(target[prop], source[prop], force);
+                }
+                else {
+                    target[prop] = source[prop];
+                }
             }
         }
     }
